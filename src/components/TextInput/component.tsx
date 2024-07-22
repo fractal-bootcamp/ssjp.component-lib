48cb743521a56dbca7164c4ba4d9b852019aa7a1
SamuelRCrider <sam.crider@aol.com>
2024-07-22 15:22:13 -0400

    onchange and value

--- src/components/TextInput/component.tsx
+++ src/components/TextInput/component.tsx
@@ -14,26 +14,35 @@ const TextInput = ({
   type,
   ...otherProps
 }: TextInputProps) => {
+  const [value, setValue] = useState<string>("");
+  // state fopr password visibility if input type is password
   const [toggleVisibility, setToggleVisibility] = useState<PasswordVisibility>(
     PasswordVisibility.Hidden
   );
+
+  // determines the input type
+  const inputType = type
+    ? // if type is password, set the toggle visibility to the password visibility state
+      type === InputType.Password
+      ? toggleVisibility
+      : InputType.Text
+    : // if type is not password, set the input type to text
+      InputType.Text;
+
   const inputDisabled = disabled ? disabled : false;
   const inputPlaceholder = placeholder ? placeholder : "Input";
   const inputSize = size ? size : InputSize.Medium;
-  const inputType = type
-    ? type === InputType.Password
-      ? toggleVisibility
-      : InputType.Text
-    : InputType.Text;
 
   return (
     <div className="h-fit w-fit flex flex-row items-center">
       <input
         type={inputType}
+        value={value}
         placeholder={inputPlaceholder}
         size={inputSize}
         disabled={inputDisabled}
         className="bg-gray-200"
+        onChange={({ target }) => setValue(target.value)}
         {...otherProps}
       />
       {type === InputType.Password && (
