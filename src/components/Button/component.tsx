91a995474733d2b1b75ccf672dce3fd9166091ec
Joshua Henderson-Jardim <hendersonjardimj@gmail.com>
2024-07-22 13:52:13 -0400

    button functionality component

--- src/components/Button/component.tsx
+++ src/components/Button/component.tsx
@@ -32,6 +32,7 @@ const Button: React.FC<ButtonProps> = ({
 
   const baseStyles = 'inline-flex items-center justify-center rounded-md font-medium transition-all duration-200 ease-in-out focus:outline-none focus:ring-offset-2';
 
+  //variant styles need to be added 
   // const variantStyles: 
 
 
