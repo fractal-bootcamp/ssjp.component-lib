b96e3633387de7b19e914f0a10629ba8a14e41c0
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-07-22 11:47:00 -0400

    tooltip functionality

--- src/components/Tooltip/component.tsx
+++ src/components/Tooltip/component.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import {useState, useRef, useEffect} from "react";
 
-const Tooltip = () => {
-  return <div>Tooltip</div>;
+//content
+//customizable positions
+//triggers on button hover
+//delays
+
+function Tooltip({children, content, position}: {content: string, position: string}) {
+  const [isVisible, setIsVisible] = useState(false)
+  const wrapperRef = useRef(null)
+
+  useEffect(() => {
+
+    const triggerElement = wrapperRef.current
+  
+    const showTooltip = () => {
+      setTimeout(()=> setIsVisible(true), 300)
+    };
+
+    const hideTooltip = () => {
+      setTimeout(()=> setIsVisible(false), 300) 
+    };
+  
+    if (triggerElement) {
+      // hover start
+      triggerElement.addEventListener('mouseenter', showTooltip);
+      // hover end
+      triggerElement.addEventListener('mouseleave', hideTooltip);
+
+      //focus events
+      triggerElement.addEventListener('focus', showTooltip);
+      triggerElement.addEventListener('blur', hideTooltip);
+    }
+    return () => {
+      if (triggerElement) {
+        //hover
+        triggerElement.removeEventListener('mouseenter', showTooltip);
+        triggerElement.removeEventListener('mouseleave', hideTooltip);
+
+        //focus 
+        triggerElement.removeEventListener('focus', showTooltip);
+        triggerElement.removeEventListener('blur', hideTooltip);
+      }
+    };
+  }, [])
+
+
+  return(
+    <div ref={wrapperRef}> 
+      {children}
+      {isVisible && (
+      <div>{content}</div> )}
+    </div> 
+  )
 };
+
 
 export default Tooltip;
